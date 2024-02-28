b802fdf278e1b42886afe5da4f814993d411d015
seongsoodevelop <119916484+seongsoodevelop@users.noreply.github.com>
2024-02-28 23:34:06 +0900

    ios 토큰 찍어보기

--- src/lib/token.js
+++ src/lib/token.js
@@ -29,6 +29,7 @@ export const decodeToken = (token) => {
 
 export const jwtMiddleware = async (ctx, next) => {
   const token = ctx.cookies.get("access_token"); // ctx 에서 access_token 을 읽어옵니다
+  console.log(token);
   if (!token) return next(); // 토큰이 없으면 바로 다음 작업을 진행합니다.
 
   try {
