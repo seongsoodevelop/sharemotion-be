bc2a5f3123ee5267e29d547f556c805f33af7a37
seongsoodevelop <119916484+seongsoodevelop@users.noreply.github.com>
2024-02-29 16:32:14 +0900

    Update cookieOptions.js

--- src/lib/option/cookieOptions.js
+++ src/lib/option/cookieOptions.js
@@ -1,14 +1,16 @@
 export const getCookieSecureOptions = (isProduction) => {
   if (isProduction) {
     return {
-      // sameSite: "none",
-      // secure: true,
+      sameSite: "Lax",
+      secure: true,
       // httpOnly: true,
+      httpOnly: false,
       maxAge: 1000 * 60 * 60 * 24 * 7,
     };
   } else {
     return {
       // httpOnly: true,
+      httpOnly: false,
       maxAge: 1000 * 60 * 60 * 24 * 7,
     };
   }
